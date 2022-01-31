bca3d8bc7335f7ff9f54e9e7a361b5a934fc5078
Marcoff90 <marcoff90@icloud.com>
2022-01-31 11:40:13 +0100

    Fixed bug with not autoincrementing id value in tricks and history which caused the appp to crash

--- src/main/java/com/greenfox/programmerspetclub/models/history/History.java
+++ src/main/java/com/greenfox/programmerspetclub/models/history/History.java
@@ -1,6 +1,8 @@
 package com.greenfox.programmerspetclub.models.history;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Data;
 
@@ -10,6 +12,7 @@ public class History {
 
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String name;
   private String description;
--- src/main/java/com/greenfox/programmerspetclub/models/trick/Trick.java
+++ src/main/java/com/greenfox/programmerspetclub/models/trick/Trick.java
@@ -1,6 +1,8 @@
 package com.greenfox.programmerspetclub.models.trick;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Data;
 
@@ -9,6 +11,7 @@ import lombok.Data;
 public class Trick {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String name;
   private String description;
